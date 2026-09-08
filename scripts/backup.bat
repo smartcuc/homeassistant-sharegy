cd4df936c0621fe65b8a51a3f1f3c2420efc0376
Ruediger Koenen <ruediger@coach4success.eu>
2026-09-08 04:11:58 +0200

    chore: update backup script

--- scripts/backup.bat
+++ scripts/backup.bat
@@ -1,5 +1,5 @@
 robocopy "c:\Users\Public\Dev\eswes" "\\tsclient\C\temp\dev-pc\eswes" *.py /S /XD venv .git frontend
-robocopy "c:\Users\Public\Dev\eswes" "\\tsclient\C\temp\dev-pc\eswes" .env
+robocopy "c:\Users\Public\Dev\eswes" "\\tsclient\C\temp\dev-pc\eswes" .env*
 robocopy "c:\Users\Public\Dev\eswes\frontend" "\\tsclient\C\temp\dev-pc\eswes\frontend" *.*
 robocopy "c:\Users\Public\Dev\eswes\frontend\src" "\\tsclient\C\temp\dev-pc\eswes\frontend\public" *.* /S 
 robocopy "c:\Users\Public\Dev\eswes\frontend\src" "\\tsclient\C\temp\dev-pc\eswes\frontend\src" *.* /S 
